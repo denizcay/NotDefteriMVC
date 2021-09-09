1e7bf61958c3c6d1891d503c752bc5776899692d
Deniz ÇAY <idenizcay@gmail.com>
2021-09-09 13:14:31 +0300

    Admin paneli layoutu eklendi

---
+++ NotDefteriMVC/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NotDefteriMVC.Areas.Admin.Models;
+using NotDefteriMVC.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotDefteriMVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "admin")]
+    public class DashboardController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public DashboardController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult Index()
+        {
+            var vm = new DashboardViewModel()
+            {
+                NoteCount = db.Notes.Count(),
+                MemberCount = db.Users.Count()
+            };
+            return View(vm);
+        }
+    }
+}
---
+++ NotDefteriMVC/Data/ApplicationDbSeed.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotDefteriMVC.Data
+{
+    public class ApplicationDbSeed
+    {
+        public static async Task SeedRolesAndUsers(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+        {
+            // admin rolü yoksa oluşturma
+            if (!await roleManager.RoleExistsAsync("admin"))
+            {
+                await roleManager.CreateAsync(new IdentityRole() { Name = "admin"});
+            }
+
+            // admin rolünde bir kullanıcı yoksa ekleme 
+            if (!await userManager.Users.AnyAsync(x => x.Email == "admin@example.com"))
+            {
+                ApplicationUser adminUser = new ApplicationUser()
+                {
+                    UserName = "admin@example.com",
+                    Email = "admin@example.com",
+                    EmailConfirmed = true
+                };
+
+                await userManager.CreateAsync(adminUser, "Ankara1.");
+
+                await userManager.AddToRoleAsync(adminUser, "admin");
+               
+            }
+
+        }
+    }
+}
